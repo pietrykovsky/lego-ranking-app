7e7184f17030b697d156b6fe1fbcbbd4768f0254
pietrykovsky <m.pietrykowski2001@gmail.com>
2023-01-15 15:09:43 +0100

    fixed scraping themes

--- backend/legoscraper/scraper.py
+++ backend/legoscraper/scraper.py
@@ -4,7 +4,8 @@ from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
-class LegoScraper():
+
+class LegoScraper:
     """Lego webstore scrapper"""
 
     def __init__(self, themes_url):
@@ -19,17 +20,19 @@ class LegoScraper():
 
     def has_next_page(self, html):
         """Return true if has next page and false otherwise."""
-        soup = BeautifulSoup(html, 'html.parser')
-        if soup.find('a', attrs={'data-test': 'pagination-next'}):
+        soup = BeautifulSoup(html, "html.parser")
+        if soup.find("a", attrs={"data-test": "pagination-next"}):
             return True
         return False
 
     def scrape_themes_urls(self):
         """Return list of lego themes urls."""
         html = self.get_html(self.themes_url)
-        soup = BeautifulSoup(html, 'html.parser')
-        list = ['https://www.lego.com' + link.get('href') for link in soup.find_all('a', class_="CategoryLeafstyles__ImagesLink-is33yg-4")]
-
+        soup = BeautifulSoup(html, "html.parser")
+        list = [
+            "https://www.lego.com" + link.get("href")
+            for link in soup.find_all("a", attrs={"data-test": "themes-link"})
+        ]
         return list
 
     def scrape_sets_urls_from_theme(self, theme_url):
@@ -37,11 +40,13 @@ class LegoScraper():
         list = []
         page = 1
         while True:
-            html = self.get_html(theme_url + f'?page={page}')
-            soup = BeautifulSoup(html, 'html.parser')
+            html = self.get_html(theme_url + f"?page={page}")
+            soup = BeautifulSoup(html, "html.parser")
 
-            for link in soup.find_all('a', attrs={'data-test': "product-leaf-title-link"}):
-                list.append('https://www.lego.com' + link.get('href'))
+            for link in soup.find_all(
+                "a", attrs={"data-test": "product-leaf-title-link"}
+            ):
+                list.append("https://www.lego.com" + link.get("href"))
 
             if not self.has_next_page(html):
                 break
@@ -53,69 +58,90 @@ class LegoScraper():
     def scrape_set(self, url):
         """Retrieve lego set fields from url and return dictionary of fields."""
         html = self.get_html(url)
-        soup = BeautifulSoup(html, 'html.parser')
-        
+        soup = BeautifulSoup(html, "html.parser")
+
         try:
-            title = soup.find(attrs={'property': 'og:title'})['content']
-            str_list = title.split('|')
-            title = str_list[0][:len(str_list[0])-1]
+            title = soup.find(attrs={"property": "og:title"})["content"]
+            str_list = title.split("|")
+            title = str_list[0][: len(str_list[0]) - 1]
             theme = str_list[1]
-            theme = theme[1:len(theme)-1]
+            theme = theme[1 : len(theme) - 1]
         except:
             title = None
             theme = None
 
         try:
-            product_id = soup.find(attrs={'property': 'product:retailer_item_id'})['content']
+            product_id = soup.find(attrs={"property": "product:retailer_item_id"})[
+                "content"
+            ]
         except:
             product_id = None
 
         try:
-            price = soup.find(attrs={'property': 'product:price:amount'})['content']
+            price = soup.find(attrs={"property": "product:price:amount"})["content"]
             price = Decimal(price)
         except:
             price = None
 
         try:
-            available = soup.find(attrs={'data-test': 'product-overview-availability'}).find('span').string
+            available = (
+                soup.find(attrs={"data-test": "product-overview-availability"})
+                .find("span")
+                .string
+            )
         except:
             available = None
 
         try:
-            age = soup.find(attrs={'data-test': 'ages-value'}).find('span').find('span').string
+            age = (
+                soup.find(attrs={"data-test": "ages-value"})
+                .find("span")
+                .find("span")
+                .string
+            )
         except:
             age = None
 
         try:
-            elements = soup.find(attrs={'data-test': 'pieces-value'}).find('span').find('span').string
+            elements = (
+                soup.find(attrs={"data-test": "pieces-value"})
+                .find("span")
+                .find("span")
+                .string
+            )
             elements = int(elements)
         except:
             elements = None
-        
+
         try:
-            minifigures = soup.find(attrs={'data-test': 'minifigures-value'}).find('span').find('span').string
+            minifigures = (
+                soup.find(attrs={"data-test": "minifigures-value"})
+                .find("span")
+                .find("span")
+                .string
+            )
             minifigures = int(minifigures)
         except:
             minifigures = None
-            
+
         try:
-            img_src = soup.find(attrs={'property': 'og:image'})['content']
-            tmp = img_src.split('?')
+            img_src = soup.find(attrs={"property": "og:image"})["content"]
+            tmp = img_src.split("?")
             img_src = tmp[0]
         except:
             img_src = None
 
         lego_set = {
-            'title': title,
-            'product_id': product_id,
-            'theme': theme,
-            'price': price,
-            'available': available,
-            'age': age,
-            'elements': elements,
-            'link': url,
-            'minifigures': minifigures,
-            'img_src': img_src,
+            "title": title,
+            "product_id": product_id,
+            "theme": theme,
+            "price": price,
+            "available": available,
+            "age": age,
+            "elements": elements,
+            "link": url,
+            "minifigures": minifigures,
+            "img_src": img_src,
         }
 
         return lego_set
--- backend/legoscraper/tests/test_scraper.py
+++ backend/legoscraper/tests/test_scraper.py
@@ -4,40 +4,45 @@ from ..scraper import LegoScraper
 
 from decimal import Decimal
 
+
 class ScraperTests(TestCase):
     """Tests for scraper module."""
-    
+
     def setUp(self):
-        themes_url = 'https://www.lego.com/pl-pl/themes/'
+        themes_url = "https://www.lego.com/pl-pl/themes/"
         self.scraper = LegoScraper(themes_url)
 
     def test_scrape_themes_urls_success(self):
         """Test function returns list of themes urls correct."""
         urls = self.scraper.scrape_themes_urls()
-        
-        self.assertIn('https://www.lego.com/pl-pl/themes/architecture', urls)
+        print(urls)
+        self.assertIn("https://www.lego.com/pl-pl/themes/architecture", urls)
 
     def test_scrape_sets_urls_from_theme_success(self):
         """Test function returns list of sets urls correct."""
-        urls = self.scraper.scrape_sets_urls_from_theme('https://www.lego.com/pl-pl/themes/speed-champions')
-        
-        self.assertIn('https://www.lego.com/pl-pl/product/007-aston-martin-db5-76911', urls)
+        urls = self.scraper.scrape_sets_urls_from_theme(
+            "https://www.lego.com/pl-pl/themes/speed-champions"
+        )
+
+        self.assertIn(
+            "https://www.lego.com/pl-pl/product/007-aston-martin-db5-76911", urls
+        )
 
     def test_scrape_set_success(self):
         """Test scrape set function returns correct values."""
-        url = 'https://www.lego.com/pl-pl/product/lego-creative-bricks-10692'
+        url = "https://www.lego.com/pl-pl/product/lego-creative-bricks-10692"
         fields = self.scraper.scrape_set(url)
         lego_set = {
-            'title': 'Kreatywne klocki LEGO® 10692',
-            'product_id': '10692',
-            'theme': 'Classic',
-            'price': Decimal("69.99"),
-            'available': 'Dostępne teraz',
-            'age': '4-99',
-            'elements': 221,
-            'link': url,
-            'minifigures': None,
-            'img_src': 'https://www.lego.com/cdn/cs/set/assets/bltdcfc11be71d04698/10692.jpg'
+            "title": "Kreatywne klocki LEGO® 10692",
+            "product_id": "10692",
+            "theme": "Classic",
+            "price": Decimal("48.99"),
+            "available": "Dostępne teraz",
+            "age": "4-99",
+            "elements": 221,
+            "link": url,
+            "minifigures": None,
+            "img_src": "https://www.lego.com/cdn/cs/set/assets/bltdcfc11be71d04698/10692.jpg",
         }
         print(fields)
         for k, v in lego_set.items():
@@ -45,7 +50,7 @@ class ScraperTests(TestCase):
 
     def test_scrape_sets_image_success(self):
         """Test scrape set with image source success."""
-        url = 'https://www.lego.com/pl-pl/product/007-aston-martin-db5-76911'
+        url = "https://www.lego.com/pl-pl/product/007-aston-martin-db5-76911"
         fields = self.scraper.scrape_set(url)
         print(fields)
-        self.assertIsNotNone(fields['img_src'])
+        self.assertIsNotNone(fields["img_src"])
