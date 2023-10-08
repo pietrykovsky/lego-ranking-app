75d13edec9da76d49d8c0c8830b9881b951b68c1
pietrykovsky <m.pietrykowski2001@gmail.com>
2023-10-08 17:03:00 +0200

    automated admin creation

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,5 @@ python manage.py wait_for_db
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
+python manage.py createsuperuser --no-input
 gunicorn api.wsgi --bind 0.0.0.0:8000
