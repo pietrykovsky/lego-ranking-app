b94007ee887915ed289e84055c7b66634bee1a54
pietrykovsky <m.pietrykowski2001@gmail.com>
2025-01-25 23:22:53 +0100

    Setup Django project, update environment variables, and restructure Docker setup

---
+++ backend/api/urls.py
@@ -0,0 +1,15 @@
+from rest_framework import routers
+
+from django.urls import path, include
+
+from api.views import LegoSetViewSet, ThemeListView, AgeCategoryListView, health_check
+
+router = routers.DefaultRouter()
+router.register('legosets', LegoSetViewSet)
+
+urlpatterns = [
+    path('health/', health_check, name='health-check'),
+    path('legosets/themes/', ThemeListView.as_view(), name='legosets-theme-list'),
+    path('legosets/age-categories/', AgeCategoryListView.as_view(), name='legosets-age-category-list'),
+    path('', include(router.urls)),
+]
