0cd81764eac31657c2bd2b38610f5fd05602e9a8
pietrykovsky <m.pietrykowski2001@gmail.com>
2025-01-25 11:46:07 +0100

    Setup backend environment with poetry

--- backend/legoscraper/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class LegoscraperConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'legoscraper'
-    verbose_name = 'Lego sets'
