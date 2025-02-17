35ad10701cf70e9ad008422fb8358617b9e7ae5d
pietrykovsky <m.pietrykowski2001@gmail.com>
2025-02-17 18:00:35 +0100

    Add loading component, navbar, and top section. Update layout and styles for Lego ranking app.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,32 +1,51 @@
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { getLegoSets } from "@/lib/actions";
+"use client";
+
+import { useState, useEffect } from "react";
+import { getLegoSets, getAgeCategories, getThemes } from "@/lib/actions";
 import LegoSetsList from "@/components/lego-sets-list";
+import TopSection from "@/components/topsection";
+import NavBar from "@/components/navbar";
+import { LegoSet, Category } from "@/lib/types";
 
 export default function LegoRanking() {
-  const legoSetsPromise = getLegoSets();
+  const [initialLegoSets, setInitialLegoSets] = useState<LegoSet[]>([]);
+  const [themes, setThemes] = useState<Category[]>([]);
+  const [ageCategories, setAgeCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [filteredLegoSets, setFilteredLegoSets] = useState<LegoSet[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      const [legoSets, themesData, ageCategoriesData] = await Promise.all([
+        getLegoSets(),
+        getThemes(),
+        getAgeCategories(),
+      ]);
+
+      setInitialLegoSets(legoSets);
+      setFilteredLegoSets(legoSets);
+      setThemes(themesData);
+      setAgeCategories(ageCategoriesData);
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
 
   return (
-    <div className="min-h-screen p-8">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-4xl font-bold text-gray-900 mb-8">LEGO Set Ranking</h1>
-
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
-          {/* Filters Sidebar */}
-          <div className="lg:col-span-3">
-            {/* LEGO Sets */}
-            <ErrorBoundary fallback={<div>Wystąpił błąd podczas ładowania zestawów LEGO</div>}>
-              <Suspense
-                fallback={
-                  <div className="w-10 h-10 border-4 border-t-red-700 border-gray-300 rounded-full animate-spin" />
-                }
-              >
-                <LegoSetsList legoSetsPromise={legoSetsPromise} />
-              </Suspense>
-            </ErrorBoundary>
+    <>
+      <NavBar />
+      <TopSection />
+      <div className="min-h-screen p-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 justify-center">
+            <div className="lg:col-span-3">
+              <LegoSetsList legoSets={filteredLegoSets} isLoading={loading} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
