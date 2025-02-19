5be72db85b97f410877e00c3ace68164ede831f4
pietrykovsky <m.pietrykowski2001@gmail.com>
2025-02-19 02:28:55 +0100

    Add CSRF_TRUSTED_ORIGINS setting based on CORS_ALLOWED_ORIGINS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,6 +29,7 @@ DEBUG = os.environ.get("DEBUG", "true").lower() == "true"
 if not DEBUG:
     CORS_ALLOWED_ORIGINS = []
     CORS_ALLOWED_ORIGINS.extend(filter(None, os.environ.get("CORS", "").split(",")))
+    CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
     ALLOWED_HOSTS = []
     ALLOWED_HOSTS.extend(filter(None, os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")))
 else:
